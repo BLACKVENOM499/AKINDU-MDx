76a0bd33de2708c42054b356cf689f2f555c87ac
AKINDU-MD-2025 <akindudimanshamd@gmail.com>
2025-06-23 01:03:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "AKINDU-MD~7J9yHT6b#EsZfgYeA5KzDtcNgJX6E_jaZtMUI4dzz1Pog6zwJRLg",
+    SESSION_ID: process.env.SESSION_ID || "7J9yHT6b#EsZfgYeA5KzDtcNgJX6E_jaZtMUI4dzz1Pog6zwJRLg",
     CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
     CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🔥,❤️‍🩹,❤️,🩷,🧡,💛,💚,💙,🩵,💜,🤎,🖤,🩶,🤍",
     CAPTION: process.env.CAPTION || "*ᴀᴋɪɴᴅᴜ-ᴍᴅ*",
